e2fef20078a03fb935a73158a914b14f26f98de6
S M Kaiser Ahmed <kaiser331042@gmail.com>
2021-11-13 14:02:02 +0600

    first commit

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizza List
+        </div>
+
+        <p>{{ $name }}</p>
+        <p>{{ $age }}</p>
+        
+        <!-- @for($i = 0; $i < 5; $i++)
+          <p>the value of i is {{ $i }}</p>
+        @endfor -->
+
+        <!-- @for($i = 0; $i < count($pizzas); $i++)
+          <p>{{ $pizzas[$i]['type'] }}</p>
+        @endfor -->
+
+        @foreach($pizzas as $pizza)
+          <div>
+            {{ $loop->index }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
+            @if($loop->first)
+              <span> - first in the loop</span>
+            @endif
+            @if($loop->last)
+              <span> - last in the loop</span>
+            @endif
+          </div>
+        @endforeach
+
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+    $pizzas = [
+        ['type' => 'hawaiin', 'base' => 'cheesy crust'],
+        ['type' => 'volcano', 'base' => 'garlic crust'],
+        ['type' => 'veg supreme', 'base' => 'thin & crispy']
+    ];
+
+    return view('pizzas', [
+        'pizzas' => $pizzas,
+        'name' => request('name'),
+        'age' => request('age')
+    ]);
+});
