514eb2a00bb07471505ffca40010ff3efb4b51af
Kaiser Ahmed <kaiser331042@gmail.com>
2021-11-16 22:43:53 +0600

    ongoing

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,12 +1,28 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Add a new pizza
-        </div>
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="" method="">
+        <label for="name">Your name:</label>
+        <input type="text" id="name" name="name">
+        
+        <label for="type">Choose pizza type:</label>
+        <select name="type" id="type">
+            <option value="margarita">Margarita</option>
+            <option value="hawaiin">Hawaiin</option>
+            <option value="veg supreme">Veg Supreme</option>
+            <option value="volcano">volcano</option>
+        </select>
 
-    </div>
+        <label for="base">Choose base type:</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="thin & crispy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <input type="submit" value="Order Pizza">
+    </form>
 </div>
 @endsection
