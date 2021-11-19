f3fc51c62279ae78c2cf175d898f3ea727f371b5
Kaiser Ahmed <kaiser331042@gmail.com>
2021-11-19 15:39:55 +0600

    ongoing

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -27,4 +27,26 @@ class PizzaController extends Controller
     public function create(){
         return view('pizzas.create');
     }
+    
+    public function store(){
+
+        $pizza = new Pizza();
+
+        $pizza->name = request('name');
+        $pizza->type = request('type');
+        $pizza->base = request('base');
+        $pizza->base = request('base');
+        $pizza->toppings = request('toppings');
+
+        $pizza->save();
+
+        return redirect('/')->with('mssg', 'Thanks for your order');
+    }
+
+    public function destroy($id){
+        $pizza = Pizza::findOrFail($id);
+        $pizza->delete();
+
+        return redirect('/pizzas');
+    }
 }
