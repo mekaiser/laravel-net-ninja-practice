efcba878a491e155c1f6f1227501e3344e0fb992
Kaiser Ahmed <kaiser331042@gmail.com>
2021-11-20 14:21:39 +0600

    ongoing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+    <div class="top-right links">
+        @auth
+        <a href="{{ url('/home') }}">Home</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}">Register</a>
+        @endif
+        @endauth
+    </div>
     @endif
 
     <div class="content">
@@ -22,7 +22,7 @@
             The North's Best Pizzas
         </div>
         <p class="mssg">{{ session('mssg') }}</p>
-        <a href="/pizzas/create">Order a Pizza</a>
+        <a href="{{ route('pizzas.create') }}">Order a Pizza</a>
     </div>
 </div>
 @endsection
